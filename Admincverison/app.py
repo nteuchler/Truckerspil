3f33852a0baa77719bbd3d197138228d674f035d
Niels Teuchler <86737398+nteuchler@users.noreply.github.com>
2025-09-10 13:24:48 +0200

    indentation error

--- Admincverison/app.py
+++ Admincverison/app.py
@@ -437,7 +437,8 @@ def delete_player():
 
 
 if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     # bind to 0.0.0.0 and use Render's $PORT
     app.run(host="0.0.0.0", port=port, debug=False)
+
 
